bd21e47277611728c97aa08e88c747e54b69f1e0
Casper Björk <erik.casper.bjork@gmail.com>
2016-12-05 22:31:01 +0100

    Snart klart med nya användare
    Snart kan man lägga till nya användare till sidan. detta har dock tagit
    lite tid men det ända som hindrar är några buggar.

--- PHP/authID.php
+++ PHP/authID.php
@@ -1,5 +1,9 @@
 <?php
 
+/**
+ * This file is for the authentication side for google currnetly. This will check if there is a user in our database with the unique ID google gives every user.
+ * @author Casper Björk <https://github.com/Cabbex>
+ */
 require_once __DIR__ . "/google-api-php-client-2.1.0/vendor/autoload.php";
 require 'connection.php';
 
@@ -21,15 +25,26 @@ if ($_GET["token"]) {
             } catch (PDOException $exc) {
                 echo "CheckUser(True) Error, Meddelande: " . $exc;
             }
-            //Byta innan man skickar upp till servern
-            header("Location: http://localhost/BikePool/getBasicUserProfile.php?id=".$result["sub"]);
+            //Byta innan man skickar upp till webbservern
+            header("Location: http://localhost/BikePool/getBasicUserProfile.php?id=" . $result["sub"]);
         } else {
             //Användaren finns inte.
-            echo "Användare finns inte";
+            $id = getNextID();
+            insertKontakt($result, $id);
+            insertPosition($id);
+            createUser($id, $result);
         }
     }
 }
 
+/**
+ * <h1>Check if the user already exists</h1>
+ * <p>Uses a SELECT question to ask if there is any data 
+ * from the $sub. Returns true if there is a user, returns false if there is none</p>
+ * @author Casper Björk <https://github.com/Cabbex>
+ * @param Google-ID $sub
+ * @return boolean
+ */
 function checkUser($sub) {
 
     try {
@@ -40,6 +55,7 @@ function checkUser($sub) {
         $resultSet = $stmt->fetch();
     } catch (PDOException $e) {
         echo "Checkuser Error, meddelande = " . $e;
+        echo "<br/>";
     }
     if ($resultSet["Login_id"] == $sub) {
         $connection = null;
@@ -52,4 +68,62 @@ function checkUser($sub) {
         $resultSet = null;
         return false;
     }
+}
+
+function insertKontakt($result, $id) {
+    try {
+        $connection = getConnection();
+        $sql = "INSERT INTO kontaktlista(Email, Nummer, Kontakt_ID) VALUES (" . $result["email"] . ",0," . $id . ")";
+        $stmt = $connection->prepare($sql);
+        $stmt->execute();
+    } catch (PDOException $exc) {
+        echo "InsertKontakt fel - " . $exc;
+        echo "<br/>";
+    }
+    $stmt = null;
+    $connection = null;
+}
+
+function insertPosition($id) {
+    try {
+        $connection = getConnection();
+        $sql = "INSERT INTO position (Longitude, Latitude, Positions_ID) VALUES (1,1," . $id . ")";
+        $stmt = $connection->prepare($sql);
+        $stmt->execute();
+    } catch (PDOException $exc) {
+        echo "InsertPosition fel - " . $exc;
+        echo "<br/>";
+    }
+    $stmt = null;
+    $connection = null;
+}
+
+function getNextID() {
+    try {
+        $connection = getConnection();
+        $sql = "SELECT Positions_ID FROM position";
+        $PosStmt = $connection->prepare($sql);
+        $PosStmt->execute();
+        $totalUsers = $PosStmt->fetchAll();
+    } catch (PDOException $exc) {
+        echo "getNextID fel - ".$exc;
+        echo "<br/>";
+    }
+
+    $id = count($totalUsers) + 1;
+    $PosStmt = null;
+    return $id;
+}
+
+function createUser($id, $result){
+    try {
+        $connection = getConnection();
+        $sql = "INSERT INTO anvandare(ID, Namn, Tillganglighet, G_F, Tagtid, KL_ID, POS_ID, Login_ID)
+            VALUES (" . $id . "," . $result["name"] . ",0,1,null," . $id . "," . $id . "," . $result["sub"] . ")";
+        $stmt = $connection->prepare($sql);
+        $stmt->execute();
+    } catch (PDOException $exc) {
+        echo "createUser fel - ".$exc;
+    }
+
 }
--- PHP/connection.php
+++ PHP/connection.php
@@ -1,5 +1,10 @@
 <?php
-
+/**
+ * <h1>Creates a connection to DB </h1>
+ * <p>This creates a PDO connection to the Database that can be modified in this method. </p>
+ *  @author Casper Björk <https://github.com/Cabbex>
+ *  @return string|\PDO
+ */
 function getConnection(){
 $servername = "localhost";
 $username = "root";
@@ -10,7 +15,6 @@ try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully";
     return $conn;
     }
 catch(PDOException $e)
